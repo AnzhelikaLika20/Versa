b935c432c89078fba3957a0251523d343264c06c
Anzhelika Gakal <asgakal@edu.hse.ru>
2024-03-31 12:50:17 +0300

    add maintaining of file versions

--- Backend/VersaProject.Dal/Repositories/Interfaces/IFileDataRepository.cs
+++ Backend/VersaProject.Dal/Repositories/Interfaces/IFileDataRepository.cs
@@ -5,4 +5,5 @@ namespace VersaProject.Dal.Repositories.Interfaces;
 public interface IFileDataRepository
 {
     public Task<int> SaveFileDataAsync(FileData fileData);
+    public Task<FileData?> GetLatestFileData(string fileName);
 }
