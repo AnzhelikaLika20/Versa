85ffc76684aa644c12dad0132b5f96bf60a4e35c
Anzhelika Gakal <asgakal@edu.hse.ru>
2024-04-01 13:16:39 +0300

    add getting files from storage

--- Backend/VersaProject.Bll/Services/Interfaces/IFileService.cs
+++ Backend/VersaProject.Bll/Services/Interfaces/IFileService.cs
@@ -7,4 +7,6 @@ public interface IFileService
 {
     public Task<PutObjectResponse> SaveFile(IFormFile file, string currentUser);
     public void StoreFileInfo(IFormFile file, string currentUser);
+    public Task<GetObjectResponse> GetFile(string fileName, int version, string currentUser);
+    public Task<string> ReadReceivedFile(GetObjectResponse response);
 }
