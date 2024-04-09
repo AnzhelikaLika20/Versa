e7aec925762e60b3f51fa4dc128d089606befaf4
Victor Frolov-Bukanov <fvictorb04@gmail.com>
2024-04-09 21:59:49 +0300

    Fixed minor bugs on main page and changed redirection mechanic from flags to Route components

--- Frontend/versa-app/src/components/SignInFrame.js
+++ Frontend/versa-app/src/components/SignInFrame.js
@@ -1,53 +1,50 @@
 import React from 'react'
 
 import {Helmet} from 'react-helmet'
-
+import { useHistory } from 'react-router-dom'
 import './../css/SignInFrame.css'
 import Button from "./Button";
 import close from './../img/ellipse.png'
 import Image from "./Image";
 import Input from "./Input";
 
-class SignInFrame extends React.Component {
-
-    render() {
-        const {onClick} = this.props
+const SignInFrame = () => {
+    const history = useHistory();
         
-        return (
-            <div className="sign-in-frame-container">
-                <Helmet>
-                    <title>Versa</title>
-                    <meta property="og:title" content="sign-inFrame - exported project"/>
-                </Helmet>
-                <div className="sign-in-frame-frame1">
-                    <div className="sign-in-frame-background"></div>
+    return (
+        <div className="sign-in-frame-container">
+            <Helmet>
+                <title>Versa</title>
+                <meta property="og:title" content="sign-inFrame - exported project"/>
+            </Helmet>
+            <div className="sign-in-frame-frame1">
+                <div className="sign-in-frame-background"></div>
 
-                    <div className="sign-in-frame-form">
-                        <Input type={"email"}
-                               placeholder={"email"} 
-                               className={"sign-in-frame-email"}
-                               errorClassName={"error-email"}
-                        />
+                <div className="sign-in-frame-form">
+                    <Input type={"email"}
+                           placeholder={"email"} 
+                           className={"sign-in-frame-email"}
+                           errorClassName={"error-email"}
+                    />
 
-                        <Input type={"password"} 
-                               placeholder={"password"} 
-                               className={"sign-in-frame-password"} 
-                               errorClassName={"error-password"}
-                        />
-                        
-                        <Image src={close}
-                               className="sign-in-frame-ellipse1"
-                               onClick={onClick}
-                        />
-                        <span className="sign-in-frame-text7">
-            <span>Log in to your personal account</span>
-          </span>
-                        <Button text="Sign in" backgroundColor="rgb(81, 208, 210)" id="sign-in-frame-button"/>
-                    </div>
+                    <Input type={"password"} 
+                           placeholder={"password"} 
+                           className={"sign-in-frame-password"} 
+                           errorClassName={"error-password"}
+                    />
+                    
+                    <Image src={close}
+                           className="sign-in-frame-ellipse1"
+                           onClick={() => history.push('/')}
+                    />
+                    <span className="sign-in-frame-text7">
+        <span>Log in to your personal account</span>
+      </span>
+                    <Button text="Sign in" backgroundColor="rgb(81, 208, 210)" id="sign-in-frame-button"/>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default SignInFrame
