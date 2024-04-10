81998f72b159d4d55aed63e9b3dd758e3ba8d513
Victor Frolov-Bukanov <fvictorb04@gmail.com>
2024-04-10 16:10:47 +0300

    Added dockerfile, deleted unnecessary .json files and started to do registration

--- Frontend/versa-app/src/components/SignUpFrame.js
+++ Frontend/versa-app/src/components/SignUpFrame.js
@@ -1,16 +1,31 @@
-import React from 'react'
-
-import {Helmet} from 'react-helmet'
-import { useHistory } from 'react-router-dom'
-
-import './../css/SignUpFrame.css'
+import React, { useState } from 'react';
+import { Helmet } from 'react-helmet';
+import { useHistory } from 'react-router-dom';
+import './../css/SignUpFrame.css';
 import Button from "./Button";
-import close from './../img/ellipse.png'
+import close from './../img/ellipse.png';
 import Image from "./Image";
 import Input from "./Input";
+import axios from "axios";
 
 const SignUpFrame = () => {
     const history = useHistory();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const registerUser = async () => {
+        console.log(email)
+        console.log(password)
+        try {
+            const response = await axios.post('http://localhost:8080/register', {
+                email: email,
+                password: password
+            });
+            console.log('Registration successful:', response.data);
+        } catch (error) {
+            console.error('Registration failed:', error);
+        }
+    };
 
     return (
         <div className="sign-up-frame-container">
@@ -20,30 +35,40 @@ const SignUpFrame = () => {
             </Helmet>
             <div className="sign-up-frame-frame1">
                 <div className="sign-up-frame-form">
-                    <Input type={"email"} 
-                           placeholder={"email"} 
-                           className={"sign-up-frame-email"}
-                           errorClassName={"error-email"}
+                    <Input
+                        type={"email"}
+                        placeholder={"email"}
+                        className={"sign-up-frame-email"}
+                        errorClassName={"error-email"}
+                        onChange={(e) => setEmail(e.target.value)}
                     />
 
-                    <Input type={"password"} 
-                           placeholder={"password"} 
-                           className={"sign-up-frame-password"}
-                           errorClassName={"error-password"}
+                    <Input
+                        type={"password"}
+                        placeholder={"password"}
+                        className={"sign-up-frame-password"}
+                        errorClassName={"error-password"}
+                        onChange={(e) => setPassword(e.target.value)}
                     />
 
-                    <Image src={close}
-                           className="sign-up-frame-ellipse1"
-                           onClick={() => history.push('/')}
+                    <Image
+                        src={close}
+                        className="sign-up-frame-ellipse1"
+                        onClick={() => history.push('/')}
                     />
                     <span className="sign-up-frame-text7">
-        <span>Create your own personal account</span>
-      </span>
-                    <Button text="Sign up" backgroundColor="rgba(254, 114, 101)" id="sign-up-frame-button"/>
+                        <span>Create your own personal account</span>
+                    </span>
+                    <Button
+                        text="Sign up"
+                        backgroundColor="rgba(254, 114, 101)"
+                        id="sign-up-frame-button"
+                        onClick={registerUser}
+                    />
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default SignUpFrame
+export default SignUpFrame;
